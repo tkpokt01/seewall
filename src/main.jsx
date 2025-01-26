bf7de3f03b022435c6857f8d2b4ae89cdce92211
tkpokt <tkpokt@hotmail.com>
2025-01-26 22:45:42 +0900

    //test1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App"; // Ensure this path is correct
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
