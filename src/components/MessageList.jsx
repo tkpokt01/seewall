a668b21fe6c1c646c5a3052305a229c35153a430
tkpokt <tkpokt@hotmail.com>
2025-01-26 22:34:08 +0900

    first commit

---
+++ src/components/MessageList.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+function MessageList({ messages }) {
+    return (
+        <div id="messages">
+            {messages.map((message, index) => (
+                <div key={index}>
+                    <strong>{message.sender}</strong>: {message.content}
+                </div>
+            ))}
+        </div>
+    );
+}
+
+// Add PropTypes validation
+MessageList.propTypes = {
+    messages: PropTypes.arrayOf(
+        PropTypes.shape({
+            sender: PropTypes.string.isRequired,
+            content: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
+
+export default MessageList;
